63380abfd834c116db8583b46f4a1752f2552b4e
Matt Turner <matturner@gmail.com>
2020-12-06 01:43:48 +0000

    Remove cruft from main(), avoid allocator crash.

--- src/main.rs
+++ src/main.rs
@@ -35,29 +35,6 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    let heap_value = Box::new(41);
-    println!("heap_value at {:p}", heap_value);
-
-    // create a dynamically sized vector
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    // create a reference counted vector -> will be freed when count reaches 0
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!(
-        "current reference count is {}",
-        Rc::strong_count(&cloned_reference)
-    );
-    core::mem::drop(reference_counted);
-    println!(
-        "reference count is {} now",
-        Rc::strong_count(&cloned_reference)
-    );
-
     #[cfg(test)]
     test_main();
 
@@ -95,7 +72,7 @@ fn cpu_info() {
     );
 
     cpu_cache_info();
-    cpu_features();
+    //cpu_features(); FIXME crashes the fixed_size_block allocator
 }
 
 fn cpu_cache_info() {
